7c547a17fceb2aad1c5fb7310c4aac305f359c7d
Michael Watts <Wattsjmichael@gmail.com>
2020-06-12 13:25:53 -0700

    change the form id to number, solves user input bug

--- js/scritps.js
+++ js/scritps.js
@@ -10,7 +10,9 @@
 
 //Split array into a string to find 3, 2, 1
 let robotSpeak = [];
-for (let i = 0; i < robotSpeak.length; i++){
+
+
+for (let i = 0; i <= "robotSpeak".length; i++){
     if(robotSpeak[i] == "3")
       robotSpeak[i] = "Won't You Be My Neighbor?";
     else if (robotSpeak[i] == "2")
