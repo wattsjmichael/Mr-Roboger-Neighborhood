cfc48954daf689c57ea6af48de778ff3f96e2474
Michael Watts <Wattsjmichael@gmail.com>
2020-06-23 21:21:04 -0700

    added .toString and .includes to function, function is working

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
 function robotSpeaks(number) {
   let output = [];
   for (i = 0; i <=number; i++){
-    //pseudo code - 1 - need to push out an array of numbers
-    // 2 Splice out the numbers
-    // 3 return the code with beep, boop, wont you be my neighbor
-    output.push(i);
+    if (i.toString().includes(3)) {
+      output.push("Wont You Be My Neighbor?!");
+    } else if (i.toString().includes(2)) {
+      output.push("boop");
+    } else if (i.toString().includes(1)) {
+      output.push("beep");
+    }  else {
+        output.push(i);
 
   }
 }
+  return output;
+};
+
 
 $(document).ready(function(){
-  $("#numberInput").submit(function(event){
+  $("#robogerForm").submit(function(event){
     event.preventDefault();
-    let numberInput = $("#numberInput").val();
-    let output = robotSpeaks(number);
-    output.forEach(function(element){
-      $("#robotSpoke").append(element);
+
+    let number = $("#numberInput").val();
+    let newArray = robotSpeaks(number);
+
+
+    $("#leetSpeak").html(newArray);
+    $("#robotSpoke").show();
     });
   });
-});
